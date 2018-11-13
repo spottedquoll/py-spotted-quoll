6c8e26caed5dc3d253d2e86731be8f4180b61306
spottedquoll <spottedquoll@protonmail.com>
2018-11-13 14:45:38 +1100

    added ielab heatmap builder

--- test_ielab_output.py
+++ test_ielab_output.py
@@ -34,3 +34,31 @@ plt.imshow(table, interpolation='none', norm=colors.LogNorm(np.min(table), np.ma
 plt.colorbar()
 plt.savefig(work_directory + 'heatmap_001_060.png', dpi=700)
 plt.clf()
+
+# SLQ with Harry's constraints
+filename = work_directory + 'table_080.h5'
+f = h5py.File(filename, 'r')
+table = np.array(f['table'])
+
+# Replace zeros
+table[table <= 0] = 0.001
+
+plt.figure()
+plt.imshow(table, interpolation='none', norm=colors.LogNorm(np.min(table), np.max(table)), cmap='plasma')
+plt.colorbar()
+plt.savefig(work_directory + 'heatmap_001_080.png', dpi=700)
+plt.clf()
+
+# CHARM fix
+filename = work_directory + 'table_468.h5'
+f = h5py.File(filename, 'r')
+table = np.array(f['table'])
+
+# Replace zeros
+table[table <= 0] = 0.001
+
+plt.figure()
+plt.imshow(table, interpolation='none', norm=colors.LogNorm(np.min(table), np.max(table)), cmap='plasma')
+plt.colorbar()
+plt.savefig(work_directory + 'heatmap_001_468.png', dpi=700)
+plt.clf()
