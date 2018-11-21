1583088c6700e226bd0f2c326b083c56ea9609c4
spottedquoll <spottedquoll@protonmail.com>
2018-11-21 16:52:10 +1100

    Added more estimators and export of model metrics

--- production_recipe/predict_recipe.py
+++ production_recipe/predict_recipe.py
@@ -2,10 +2,12 @@ import h5py
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
-#from sklearn.ensemble import RandomForestRegressor
-#from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingClassifier
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.kernel_ridge import KernelRidge
 
 work_directory = '/Volumes/slim/2017_ProductionRecipes/'
 
@@ -20,7 +22,10 @@ print('Read ' + str(table.shape[0]) + ' records')
 header = ['y', 'i', 'j', 'country', 'year', 'margin']
 df = pd.DataFrame(table, columns=header)
 
+# cleaning
 print('Max Aij: ' + str(df['y'].max()) + ', min Aij: ' + str(df['y'].min()))
+df = df[df.y <= 1]
+df = df[df.y >= 0]
 
 # One hot encode categorical variables
 df2 = pd.get_dummies(df, columns=['country', 'margin'])
@@ -30,9 +35,15 @@ y = np.array(df2['y'])
 X = np.array(df2.drop('y', axis=1))
 
 # Split training and validation sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-max_depth_options = [2, 5, 10]
+results = []
+
+# Decision tree model
+model_type = 'decision_tree'
+print('Building ' + model_type + ' models')
+
+max_depth_options = [2, 5, 10, None]
 min_samples_leaf_options = [1, 5, 10]
 max_features_options = ['auto', 'sqrt']
 
@@ -43,10 +54,100 @@ for md in max_depth_options:
             regressor.fit(X_train, y_train)
             y_pred = regressor.predict(X_test)
 
-            print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
-            print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
-            print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
-            print('.')
+            results.append({'model': model_type, 'hyper-parameters': [md, ms, mf]
+                            , 'mae': metrics.mean_absolute_error(y_test, y_pred)
+                            , 'mse': metrics.mean_squared_error(y_test, y_pred)
+                            , 'rmse': np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+                            , 'r2': metrics.r2_score(y_test, y_pred)
+                            })
 
-# RandomForestRegressor(max_features=2, min_samples_split=4, n_estimators=50, min_samples_leaf=2)
-# LinearRegression,Ridge,KNeighborsRegressor,DecisionTreeRegressor,RandomForestRegressor, GradientBoostingRegressor
+# random forest
+model_type = 'random_forest'
+print('Building ' + model_type + ' models')
+
+n_estimators_options = [50, 100]  # 200
+for md in max_depth_options:
+    for ms in min_samples_leaf_options:
+        for mf in max_features_options:
+            for ne in n_estimators_options:
+                regressor = RandomForestRegressor(max_depth=md, min_samples_leaf=ms, max_features=mf, n_estimators=ne)
+                regressor.fit(X_train, y_train)
+                y_pred = regressor.predict(X_test)
+
+                results.append({'model': model_type, 'hyper-parameters': [md, ms, mf, ne]
+                                , 'mae': metrics.mean_absolute_error(y_test, y_pred)
+                                , 'mse': metrics.mean_squared_error(y_test, y_pred)
+                                , 'rmse': np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+                                , 'r2': metrics.r2_score(y_test, y_pred)
+                                })
+
+# ridge regression
+fit_intercept_opts = [True, False]
+alpha_opts = [1e-15, 1e-10, 1e-8, 1e-4, 1e-3, 1e-2, 1, 5, 10, 20]
+normalise_opts = [True, False]
+
+model_type = 'ridge_regression'
+print('Building ' + model_type + ' models')
+
+for fi in fit_intercept_opts:
+    for a in alpha_opts:
+        for nm in normalise_opts:
+            regressor = Ridge(alpha=a, normalize=nm, fit_intercept=fi)
+            regressor.fit(X_train, y_train)
+            y_pred = regressor.predict(X_test)
+
+            results.append({'model': model_type, 'hyper-parameters': [fi, a, nm]
+                            , 'mae': metrics.mean_absolute_error(y_test, y_pred)
+                            , 'mse': metrics.mean_squared_error(y_test, y_pred)
+                            , 'rmse': np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+                            , 'r2': metrics.r2_score(y_test, y_pred)
+                            })
+
+# k nearest neighbors
+n_neighbors_opts = [2, 5, 10]
+weights_opts = ['uniform', 'distance']
+algorithm_opts = ['auto', 'ball_tree', 'kd_tree']
+
+model_type = 'k_nearest_neighbors'
+print('Building ' + model_type + ' models')
+
+for n in n_neighbors_opts:
+    for w in weights_opts:
+        for a in algorithm_opts:
+            regressor = KNeighborsRegressor(n_neighbors=n, weights=w, algorithm=a)
+            regressor.fit(X_train, y_train)
+            y_pred = regressor.predict(X_test)
+
+            results.append({'model': model_type, 'hyper-parameters': [n, w, a]
+                            , 'mae': metrics.mean_absolute_error(y_test, y_pred)
+                            , 'mse': metrics.mean_squared_error(y_test, y_pred)
+                            , 'rmse': np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+                            , 'r2': metrics.r2_score(y_test, y_pred)
+                            })
+
+# kernel ridge regression
+kernel_opts = ['linear', 'poly', 'rbf']
+gamma_opts = [None, 0.1, 1, 10]
+alpha_opts = [1e-4, 1e-3, 1e-2, 1, 5, 10]
+
+model_type = 'kernel_ridge_regression'
+print('Building ' + model_type + ' models')
+
+for k in kernel_opts:
+    for g in gamma_opts:
+        for a in alpha_opts:
+            regressor = KernelRidge(kernel=k, gamma=g, alpha=a)
+            regressor.fit(X_train, y_train)
+            y_pred = regressor.predict(X_test)
+
+            results.append({'model': model_type, 'hyper-parameters': [k, g, a]
+                            , 'mae': metrics.mean_absolute_error(y_test, y_pred)
+                            , 'mse': metrics.mean_squared_error(y_test, y_pred)
+                            , 'rmse': np.sqrt(metrics.mean_squared_error(y_test, y_pred))
+                            , 'r2': metrics.r2_score(y_test, y_pred)
+                            })
+
+
+# Save results
+result_df = pd.DataFrame(results, columns=['model', 'options', 'mae', 'mse', 'rmse', 'r2'])
+result_df.to_csv(work_directory + '/results/' + 'summary_results.csv', encoding='utf-8')
